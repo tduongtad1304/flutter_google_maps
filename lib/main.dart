e3c6ed855957f6ac0f9028ecb87073489a76264d
TAD <dothaiduong9a1@gmail.com>
2022-03-04 11:51:46 +0700

    update dependencies & maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,89 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class MapSample extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MapSample> createState() => MapSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  //default target location
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(16.027832, 108.220177),
+    zoom: 16.4746,
+  );
+
+  //school target location
+  static final CameraPosition _kSchool = CameraPosition(
+      bearing: 180,
+      target: LatLng(16.032010, 108.221182),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        centerTitle: true,
+        leading: Icon(Icons.arrow_back_ios),
+        title: Text(
+          'Google Maps',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: Row(
+        children: [
+          SizedBox(
+            width: 45,
+          ),
+          FloatingActionButton.extended(
+            onPressed: _goToTheSchool,
+            label: Text('Go to school!'),
+            icon: Icon(Icons.school),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton.extended(
+            onPressed: _backToTheOrigin,
+            label: Text('Go home!'),
+            icon: Icon(Icons.home),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> _goToTheSchool() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
+  }
+
+  Future<void> _backToTheOrigin() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex));
   }
 }
