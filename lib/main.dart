e830dfda8be8f333fc307fc49b6864450fbcd388
TAD <dothaiduong9a1@gmail.com>
2022-03-09 00:14:50 +0700

    directions api & get directions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps/services/location_services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -24,50 +25,89 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
-  TextEditingController _searchController = TextEditingController();
+  TextEditingController _originController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
+
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polygonLatLngs = <LatLng>[];
+  int _polygonIdCounter = 1;
+  int _polylineIdCounter = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _setDefaultMarker(LatLng(16.027832, 108.220177));
+  }
+
+  void _setDefaultMarker(LatLng point) {
+    setState(() {
+      _markers.add(
+        Marker(markerId: MarkerId('marker'), position: point),
+      );
+    });
+  }
+
+  void _setPolygons() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polygons.add(
+      Polygon(
+        polygonId: PolygonId(polygonIdVal),
+        points: polygonLatLngs,
+        strokeWidth: 2,
+        fillColor: Colors.transparent,
+      ),
+    );
+  }
+
+  void _setPolylines(List<PointLatLng> points) {
+    final String polylineIdVal = 'polyline_$_polylineIdCounter';
+    _polylineIdCounter++;
+
+    _polylines.add(
+      Polyline(
+        polylineId: PolylineId(polylineIdVal),
+        width: 2,
+        color: Colors.blue,
+        points: points
+            .map(
+              (point) => LatLng(point.latitude, point.longitude),
+            )
+            .toList(),
+      ),
+    );
+  }
 
   //default target location
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(16.027832, 108.220177),
-    zoom: 18.4746,
-  );
+  // static final CameraPosition _kGooglePlex = CameraPosition(
+  //   target: LatLng(16.027832, 108.220177),
+  //   zoom: 18.4746,
+  // );
 
   //school target location
-  static final CameraPosition _kSchool = CameraPosition(
-      bearing: 180,
-      target: LatLng(16.032010, 108.221182),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  //generating marker
-  //origin marker
-  static final Marker _kGooglePlexMarker = Marker(
-    markerId: MarkerId('_kGooglePlex'),
-    infoWindow: InfoWindow(title: 'My home'),
-    icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(16.027832, 108.220177),
-  );
+  // static final CameraPosition _kSchool = CameraPosition(
+  //     bearing: 180,
+  //     target: LatLng(16.032010, 108.221182),
+  //     tilt: 59.440717697143555,
+  //     zoom: 19.151926040649414);
 
   //school marker
-  static final Marker _kSchoolMarker = Marker(
-    markerId: MarkerId('_kSchool'),
-    infoWindow: InfoWindow(title: 'Dong A University'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
-    position: LatLng(16.032010, 108.221182),
-  );
-
-  // //draw polylines in the maps
-  // static final Polyline _kPolyline = Polyline(
-  //   consumeTapEvents: true,
-  //   polylineId: PolylineId('_kPolyline'),
-  //   points: [
-  //     LatLng(16.027832, 108.220177),
-  //     LatLng(16.032010, 108.221182),
-  //   ],
-  //   width: 3,
-  //   color: Color.fromARGB(255, 69, 127, 175),
-  //   jointType: JointType.round,
+  // static final Marker _kSchoolMarker = Marker(
+  //   markerId: MarkerId('_kSchool'),
+  //   infoWindow: InfoWindow(title: 'Dong A University'),
+  //   icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+  //   position: LatLng(16.032010, 108.221182),
   // );
+
+//generating marker
+  //origin marker
+  static final CameraPosition _kGooglePlexMarker = CameraPosition(
+    target: LatLng(16.027832, 108.220177),
+    zoom: 19.5876,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -84,43 +124,65 @@ class MapSampleState extends State<MapSample> {
           Row(
             children: [
               Expanded(
-                child: TextFormField(
-                  controller: _searchController,
-                  textCapitalization: TextCapitalization.words,
-                  textInputAction: TextInputAction.newline,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(
-                      left: 10,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _originController,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 8.0),
+                          hintText: ' Origin'),
+                      onChanged: (value) {
+                        print(value);
+                      },
                     ),
-                    hintText: 'Search by location...',
-                    hintStyle: TextStyle(
-                      color: Colors.black54,
+                    TextFormField(
+                      controller: _destinationController,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 8.0),
+                          hintText: ' Destination'),
+                      onChanged: (value) {
+                        print(value);
+                      },
                     ),
-                  ),
-                  onChanged: (value) => print(value),
+                  ],
                 ),
               ),
               IconButton(
-                  onPressed: () {
-                    LocationService().getPlaceId(_searchController.text);
-                  },
-                  icon: Icon(Icons.search))
+                onPressed: () async {
+                  var directions = await LocationServices().getDirections(
+                    _originController.text,
+                    _destinationController.text,
+                  );
+                  _goToPlace(
+                    directions['start_location']['lat'],
+                    directions['start_location']['lng'],
+                    directions['bounds_ne'],
+                    directions['bounds_sw'],
+                  );
+
+                  _setPolylines(directions['polyline_decoded']);
+                },
+                icon: Icon(Icons.search),
+              ),
             ],
           ),
           Expanded(
             child: GoogleMap(
-              mapType:
-                  MapType.hybrid, //4 types: normal, hybrid, terrain & satellite
-              markers: {
-                _kGooglePlexMarker,
-                _kSchoolMarker,
-              },
-              // polylines: {
-              //   _kPolyline,
-              // },
-              initialCameraPosition: _kGooglePlex,
+              mapType: MapType.hybrid,
+              markers: _markers,
+              polygons: _polygons,
+              polylines: _polylines,
+              initialCameraPosition: _kGooglePlexMarker,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
+              },
+              onTap: (point) {
+                setState(() {
+                  polygonLatLngs.add(point);
+                  _setPolygons();
+                });
               },
             ),
           ),
@@ -129,25 +191,27 @@ class MapSampleState extends State<MapSample> {
     );
   }
 
-  Future<void> _goToTheSchool() async {
+  Future<void> _goToPlace(
+    double lat,
+    double lng,
+    Map<String, dynamic> boundsNe,
+    Map<String, dynamic> boundsSw,
+  ) async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
-  }
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 14.667),
+      ),
+    );
 
-  Future<void> _backToTheOrigin() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex));
-  }
-
-  Future<void> _viewArea() async {
-    final GoogleMapController controller = await _controller.future;
     controller.animateCamera(
       CameraUpdate.newLatLngBounds(
           LatLngBounds(
-            southwest: LatLng(16.027832, 108.220177),
-            northeast: LatLng(16.032010, 108.221182),
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
           ),
-          48.0),
+          30),
     );
+    _setDefaultMarker(LatLng(lat, lng));
   }
 }
--- lib/services/location_services.dart
+++ lib/services/location_services.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
-class LocationService {
+class LocationServices {
   final String key = 'AIzaSyAVznY9puJ_u_HGErIFS8o1v7IX25P-1aw';
 
   Future<String> getPlaceId(String input) async {
@@ -26,6 +27,29 @@ class LocationService {
     var results = json['result'] as Map<String, dynamic>;
 
     print(results);
+    return results;
+  }
+
+  Future<Map<String, dynamic>> getDirections(
+      String origin, String destination) async {
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&key=$key';
+
+    var response = await http.get(Uri.parse(url));
+    var json = convert.jsonDecode(response.body);
+
+    var results = {
+      'bounds_ne': json['routes'][0]['bounds']['northeast'],
+      'bounds_sw': json['routes'][0]['bounds']['southwest'],
+      'start_location': json['routes'][0]['legs'][0]['start_location'],
+      'end_location': json['routes'][0]['legs'][0]['end_location'],
+      'polyline': json['routes'][0]['overview_polyline']['points'],
+      'polyline_decoded': PolylinePoints()
+          .decodePolyline(json['routes'][0]['overview_polyline']['points']),
+    };
+
+    print(results);
+
     return results;
   }
 }
